636654f7f649aa3bb0c9bc4832cd41f74cc5f2ec
dannunziof <franciscodannunzio5@gmail.com>
2019-09-08 21:45:30 -0300

    ignorar, cambios innecesarios

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>AGABOO News</h1>
+        <h1>AGABOO News undostres4</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
