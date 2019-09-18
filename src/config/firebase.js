effc9db6341447d134793f5fe8afb56fc339f2a8
dforce2055 <dperez2055@gmail.com>
2019-09-18 15:12:26 -0300

    Oculto config de firebase

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,26 +1,21 @@
 import firebase from "firebase";
 
-// Acá va la config de tu firebase
-const firebaseConfigDforce = {
-    apiKey: "AIzaSyBWyiW40BmzESznSeF__4xnX2xyLEceV-4",
-    authDomain: "agaboodforce.firebaseapp.com",
-    databaseURL: "https://agaboodforce.firebaseio.com",
-    projectId: "agaboodforce",
-    storageBucket: "agaboodforce.appspot.com",
-    messagingSenderId: "773704679477",
-    appId: "1:773704679477:web:0e58f47db6e7671aeb717c"
-};
-const firebaseConfigFrancisco = {
-    apiKey: "AIzaSyDZlLzbUPiXEscYPqEtY3G1o2HIqNSWO0E",
-    authDomain: "agaboofront-francisco.firebaseapp.com",
-    databaseURL: "https://agaboofront-francisco.firebaseio.com",
-    projectId: "agaboofront-francisco",
-    storageBucket: "",
-    messagingSenderId: "817185700205",
-    appId: "1:817185700205:web:89d2bcbc26586291fb8264"
-};
+/**
+ * acá va tu config de Firebase, en un archivo firebase-config.js ignorado por git
+ * export const firebaseConfig = {
+ *   apiKey: "apiKey",
+ *   authDomain: "TU-DOMINIO.firebaseapp.com",
+ *   databaseURL: "https://TU-DOMINIO.firebaseio.com",
+ *   projectId: "TU-DOMINIO",
+ *   storageBucket: "TU-DOMINIO.appspot.com",
+ *   messagingSenderId: "MENSAJE_ID",
+ *   appId: "appID"
+ *   };
+ *
+ */
+import  { firebaseConfig } from './firebase-config'; 
 
-const firebaseApp = firebase.initializeApp(firebaseConfigDforce);
+const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 
 export { db, firebaseApp };
