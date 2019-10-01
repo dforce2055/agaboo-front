66246a2a6bea9e789f65fcdafd5f86bc48928420
dannunziof <franciscodannunzio5@gmail.com>
2019-09-30 23:25:06 -0300

    barra a utilizar: Header/Navigation.js

---
+++ src/config/firebase-config.js
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey:'AIzaSyDLVYnr_2LjpA8aYS8ycRUllV5DTudWs7Q',
+    authDomain:'agaboodb.firebaseapp.com',
+    databaseURL:'https://agaboodb.firebaseapp.com',  
+    storageBucket: 'agaboodb.firebaseio.appspot.com',
+    projectId:'agaboodb',
+    messaginSendID:'892253944017'
+    };
