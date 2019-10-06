c7dbc1e4f2a2f7b4fa145d83d6d7e36596d9013e
Ivan <cuadradoivan1990@gmail.com>
2019-10-06 19:29:46 -0300

    Dejando un poco mas ordenado

--- src/views/MainMenu/MainMenu.js
+++ src/views/MainMenu/MainMenu.js
@@ -8,6 +8,16 @@ import Dashboard from '../Dashboard/Dashboard';
 import firebase from '../../config/firebase';
 import { withRouter } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
+import NavBar from '../Navigation';
+import ButtonSimple from '../ButtonsMenu/ButtonSimple';
+import ButtonClient from '../ButtonsMenu/ButtonClient'
+import ButtonProduct from '../ButtonsMenu/ButtonProduct';
+import ButtonStock from '../ButtonsMenu/ButtonStock';
+import ButtonOrder from '../ButtonsMenu/ButtonOrder';
+import Chart from '../Statistics/Chart';  
+import '../MainMenu/MainMenu.js';
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+
 
 const styles = theme => ({
       MuiAppBar: {
@@ -28,6 +38,30 @@ const styles = theme => ({
     },
   });
 
+  const theme2 = createMuiTheme({ /* Plantilla de edicion */
+    overrides: { 
+      MuiAppBar: {
+        colorPrimary: {
+          backgroundColor: '#3fb5a5',
+        }
+      },
+      MuiButton: {
+        containedPrimary: {
+          backgroundColor: '#3fb5a5',
+          '&:hover': {
+            backgroundColor: '#0ce8ca',
+            "@media (hover: none)": {
+              backgroundColor: "#0ce8ca"
+            },
+          },
+        },
+        fullWidth: {
+            width: '200%'
+        },
+    },
+
+  }});
+
 function MainMenu(props){
   
     
@@ -39,10 +73,24 @@ function MainMenu(props){
       }
 
         return(
-            <div>
-                  <Dashboard />
-            </div>
-          )
+              <React.Fragment>
+                <NavBar/>
+                <Chart></Chart>
+                <MuiThemeProvider theme={theme2}>
+                  <div  className = {"Button"}><ButtonClient fullWidth/></div>
+                  <div  className = {"Button"}><ButtonProduct fullWidth/></div>
+                  <div  className = {"Button"}><ButtonStock fullWidth/></div>
+                  <div  className = {"Button"}><ButtonOrder fullWidth/></div>
+                  <div  className = {"Button"}><ButtonSimple nombre = {'Estado de cuenta'/*Utilizo los props para reutilizar el boton simple*/} fullWidth/></div>
+                  <div  className = {"Button"}><ButtonSimple nombre = {'Mantenimientos a realizar'} fullWidth/></div>
+                </MuiThemeProvider>
+
+
+
+
+
+              </React.Fragment>
+                        )
 }
 
 export default withRouter(withStyles(styles)(MainMenu))
