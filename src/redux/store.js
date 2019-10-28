a2689f59fff2a70e69066847654c4796958761b4
cuadradoivan1990 <cuadradoivan1990@gmail.com>
2019-10-28 00:11:10 -0300

    Empezando con Redux

---
+++ src/redux/store.js
@@ -0,0 +1,11 @@
+import { createStore, combineReducers } from 'redux'
+//import todoApp from './reducers'
+
+const reducers = combineReducers({
+// Lugar para agregar los reducers
+
+})
+
+
+let store = createStore(todoApp)
+
