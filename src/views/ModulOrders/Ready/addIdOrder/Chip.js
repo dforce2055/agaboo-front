eb72558092a93ea91ae8ee2af822fc6f11d29ec8
Cesar Vega <vegac0189@gmail.com>
2019-11-06 13:10:50 -0300

    Agrego paginacion en la tabla de clientes

--- src/views/ModulOrders/Ready/addIdOrder/Chip.js
+++ src/views/ModulOrders/Ready/addIdOrder/Chip.js
@@ -18,11 +18,10 @@ export default function Chip(props){
     
     let auth = chip[chip.length-1] //Selecciono el ultimo dato introducido
 
-    let d =OrderController.verifyProductExistence(auth);
-console.log(d);
-
-
-    //handleChange(obj.producto,data); //Metodo para guardar el id en el listado del pedido id's
+    OrderController.verifyProductExistence(auth)
+    .then(result => console.log(result))
+    
+    handleChange(obj.producto,data); //Metodo para guardar el id en el listado del pedido id's
   }
 
  return (
