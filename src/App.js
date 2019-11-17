6533f87625bd301a3a6a8f73c26bedb5fc1a5b1b
Cesar Vega <vegac0189@gmail.com>
2019-11-17 17:08:41 -0300

    Reparo errores de importacion

--- src/App.js
+++ src/App.js
@@ -21,7 +21,7 @@ import { CssBaseline } from '@material-ui/core'
 import firebase from '../src/config/firebase';
 import LoaderScreen from './views/LoaderScreen/LoaderScreen';
 
-import AccountStatus from './views/ModuleOrder/AccountStatus/Index';
+import AccountStatus from './views/ModuleOrder/AccountStatus/index';
 import Service from './views/ModuleService/Index.js'
 import CustomizedTables from './views/ModuleOrder/Ready/Add-ID/ProductListOrder';
 
