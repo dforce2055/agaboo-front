ddf530b46d59c2ff976c3612e73f2aafe42ff6f4
Cesar Vega <vegac0189@gmail.com>
2019-11-17 20:29:25 -0300

    Funcionando chart de menu principal

--- src/controllers/Order.js
+++ src/controllers/Order.js
@@ -96,8 +96,8 @@ class OrderController extends Component {
   }
 
   allDepositsPerYear(){
-    try {
-      return OrderRepo.allDepositsPerYear();
+    try {      
+      return OrderRepo.allDepositsPerYear(); //Retorno el monto mensual
     } catch (error) {
       console.error("Error en el controlador de pedidos al enviar depositos.");      
     }
