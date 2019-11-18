311f493ac5e2d6ca795feb96f50b196a54deccb2
dforce2055 <dperez2055@gmail.com>
2019-11-18 16:21:47 -0300

    test gitlab

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,17 +1,17 @@
     
-//import { firebaseConfig } from './firebase-config';
+import { firebaseConfig } from './firebase-config';
 import app from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firebase-firestore';
 //import 'firebase-admin';
-
+/*
 let firebaseConfig;
 if (process.env.NODE_ENV === 'test') {
     firebaseConfig = process.env.FIREBASE_CONFIG;
 } else {
     firebaseConfig = process.env.FIREBASE_CONFIG_GITLAB;
 }
-
+*/
  
 class Firebase {
     constructor() {
