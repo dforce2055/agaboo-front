096c732d20dbff553f5c116e395be99aa96a1545
dforce2055 <dperez2055@gmail.com>
2019-11-18 16:23:49 -0300

    Oculto boton query

--- src/views/ModuleOrder/Ready/OrdersTable.js
+++ src/views/ModuleOrder/Ready/OrdersTable.js
@@ -311,9 +311,9 @@ function EnhancedTable(props) {
     <Fab color="primary" aria-label="add" className={classes.fab} onClick={() => history.push('/registrarPedido')} >
       <AddIcon />
     </Fab>   
-    {/* TETS */}
+    {/* TETS 
     <Button variant="contained" color="primary" onClick={()=>query('2019-11-08','2019-11-11')}>QUERY</Button>
-
+    */}
     <div className={classes.root}>
       <Paper className={classes.paper}>
         <EnhancedTableToolbar numSelected={selected.length} />
